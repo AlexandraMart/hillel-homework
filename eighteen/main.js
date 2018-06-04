d6985e9ab77656c5904002ac5a65c296e1942d51
Aleksandra <aleksandranovember11@gmail.com>
2018-06-04 03:56:50 +0300

    - 18 try

--- eighteen/main.js
+++ eighteen/main.js
@@ -1,7 +1,59 @@
 window.onload = function() {
-    product = document.querySelectorAll('.product');
-    product.addEventListener("click", function(){
-        var t = this.innerHTML;
-        console.log(t);
+    addToCard = document.querySelectorAll('.add-to-card');
+    removeFromCard = document.querySelectorAll('.remove');
+
+
+    trash = document.querySelector('.trash');
+
+    this.addToCard.forEach(function(item, i, arr){
+        item.addEventListener("click", addProductToCard);
+
     });
+
+    var cartData = {};
+
+    function addProductToCard(){
+        t = this;
+        console.log(t);
+        productName = this.parentElement.querySelector('.product-name').innerText;
+        idValue = this.parentElement.id;
+        quantity = this.parentElement.dataset.quantity;
+
+        console.log(quantity);
+        
+        
+        
+        if(!cartData[idValue]){
+         cartData[idValue] = idValue;
+
+         product = {};
+         product.name = productName;
+         product.quantity = 1;
+         
+         
+     } else {
+
+        
+         console.log("CCC");
+
+         console.log(cartData[idValue].quantity);
+         
+         y = cartData[idValue].quantity;
+         y++;
+         cartData[idValue].quantity = y;
+
+         
+     }
+
+     cartData[idValue] = product;
+
+     console.log(cartData);
+     
+     var str = JSON.stringify(cartData);
+     
+     console.log(str);
+     
+     
+
+ }
 }
