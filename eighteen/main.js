ae5b3ca8fb9c525ae204a10466a8b847c928d46f
Aleksandra <aleksandranovember11@gmail.com>
2018-06-06 17:28:36 +0300

    - 18 homework

--- eighteen/main.js
+++ eighteen/main.js
@@ -2,26 +2,35 @@ window.onload = function() {
     addToCard = document.querySelectorAll('.add-to-card');
     removeFromCard = document.querySelectorAll('.remove');
 
+    clearBasket = document.querySelector('.remove-all');
+    clearBasket.addEventListener("click", removeAll)
 
-    trash = document.querySelector('.trash');
+    basket = document.querySelector('.basket-elements');
 
-    this.addToCard.forEach(function(item, i, arr){
-        item.addEventListener("click", addProductToCard);
-
+    addToCard.forEach(function(item, i, arr){
+        item.addEventListener("click", function(){
+            addProductToCard.call(this, i);
+        });
     });
 
-    var cartData = {};
+    removeFromCard.forEach(function(item, i, arr){
+        item.addEventListener("click", function(){
+            removeProductFromCard.call(this, i);
+        });
+    });
 
-    function addProductToCard(){
-        t = this;
-        console.log(t);
-        productName = this.parentElement.querySelector('.product-name').innerText;
-        idValue = this.parentElement.id;
-        quantity = this.parentElement.dataset.quantity;
+    if(localStorage.getItem("myProduct")){
+        var cartData = JSON.parse(localStorage.myProduct);
+        showBasket();
+    } else {
+         var cartData = {};
+    }
 
-        console.log(quantity);
-        
-        
+    function addProductToCard(id){
+        productName = this.dataset.productName;
+
+        idValue = id;
+
         
         if(!cartData[idValue]){
          cartData[idValue] = idValue;
@@ -29,31 +38,53 @@ window.onload = function() {
          product = {};
          product.name = productName;
          product.quantity = 1;
-         
-         
+         cartData[idValue] = product;
      } else {
+        cartData[idValue].quantity++;
+    }
 
-        
-         console.log("CCC");
+        showBasket();
+    }
 
-         console.log(cartData[idValue].quantity);
-         
-         y = cartData[idValue].quantity;
-         y++;
-         cartData[idValue].quantity = y;
+    function showBasket(){
+        var serialObj = JSON.stringify(cartData);
+        localStorage.setItem("myProduct", serialObj);
+        var showInfo = JSON.parse(localStorage.getItem("myProduct"));
 
-         
-     }
+        if(showInfo != null){
+            basket.innerHTML = '';
 
-     cartData[idValue] = product;
+            for(var key in showInfo){
 
-     console.log(cartData);
-     
-     var str = JSON.stringify(cartData);
-     
-     console.log(str);
-     
-     
+                var productNameAdd = document.createElement('div');
+                productNameAdd.innerHTML = cartData[key].name;
+                basket.appendChild(productNameAdd);
 
- }
+                var productQuantityAdd = document.createElement('div');
+                productQuantityAdd.innerHTML = cartData[key].quantity;
+                basket.appendChild(productQuantityAdd);
+            }
+        }
+    }
+
+    function isEmpty(obj) {
+        return Object.keys(obj).length === 0;
+    }
+
+    function removeProductFromCard(id) {
+        if(!isEmpty(cartData) && cartData[idValue] != null){
+            if(cartData[idValue].quantity > 1){
+                cartData[idValue].quantity--;
+            } else {
+                delete cartData[idValue];
+            }
+            showBasket();
+        } 
+    }
+
+    function removeAll() {
+        cartData = {};
+
+        showBasket();
+    }
 }
